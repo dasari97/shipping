987c30bddaaf83322f02313287274f137d17b938
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-09 12:30:50 +0000

    adding new file

---
+++ ami/ami.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+backend "s3" {
+    bucket = "dasari1998"
+    key    = "ami/shipping/dev/dev_state_file_backup"
+    region = "us-east-1"
+}
+
+module "shipping" {
+    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-ami"
+    component   = "shipping"
+    APP_VERSION = var.APP_VERSION
+}
+
+variable "APP_VERSION" {}
