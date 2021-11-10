97c736e6af0ddd5fb544e5994916025fd496c384
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-10 14:11:27 +0000

    adding new file

---
+++ Terraform-Immutable/env/dev_backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "dasari1998"
+key    = "Terraform-immutable/shipping/dev/dev_state_file_backup"
+region = "us-east-1"
---
+++ Terraform-Immutable/main.tf
@@ -0,0 +1,10 @@
+module "cart" {
+    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//APP-module"
+    env = var.env
+    instance_type = var.instance_type
+    component   = "shipping"
+    port        = 8080
+#    is_internal = "true"
+#    priority    = 200
+    APP_VERSION = var.APP_VERSION
+}
