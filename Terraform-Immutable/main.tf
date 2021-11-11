f2eabcf15f02db99a4500415fa422de258507e60
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 11:42:37 +0000

    adding new file

--- Terraform-Immutable/main.tf
+++ Terraform-Immutable/main.tf
@@ -1,5 +1,5 @@
 module "shipping" {
-    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//APP-module"
+    source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/immutable//app-module"
     env = var.env
     instance_type = var.instance_type
     component   = "shipping"
@@ -7,4 +7,7 @@ module "shipping" {
     is_internal = "true"
     priority    = 200
     APP_VERSION = var.APP_VERSION
+    max_size    = var.max_size
+    min_size    = var.min_size
+    desired_capacity    = var.desired_capacity
 }
